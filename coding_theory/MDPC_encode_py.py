99f9f666df2a0a2a8867b2a458c7bc98ac87b16c
awlaplace <52106430+awlaplace@users.noreply.github.com>
2021-06-17 21:34:50 +0900

    MDPC_encode by python

---
+++ coding_theory/MDPC_encode_py.py
@@ -0,0 +1,88 @@
+"""
+k と n を引数にとって，k × n のMDPC符号の生成行列をランダムに生成し，
+符号語を列挙するスクリプト
+LDPC符号のスクリプトで O(1) の部分を O(√n) に変えるだけ
+"""
+
+import math
+import random
+import numpy as np
+import sys
+
+
+def list_weight(lis: list) -> int:
+    w = 0
+    for ele in lis:
+        w += ele 
+    return w
+
+
+def LDPC_encode(k: str, n: str) -> None:
+    k = int(k)
+    n = int(n)
+    
+    ## MDPC符号のHamming重みは O(√n) で，ランダムに生成する
+    ## [max(√n - log n, 1), min(√n + log n, n)] の範囲で考える
+    exists = False
+    while exists == False:
+        range_lower = max(int(math.sqrt(n) - math.log2(n)), 1)
+        range_higher = min(int(math.sqrt(n) + math.log2(n)), n)
+        weight = random.randint(range_lower, range_higher)
+        if(weight > 0):
+            if(weight <= k and weight <= n):
+                exists = True
+    
+    print("Hamming重みは", weight)
+
+    ## 各列が weight のHamming重みの生成行列をランダムに生成する
+    matrix_trans = []
+    for column in range(n):
+        column_vec = [0 for i in range(k)]
+        while list_weight(column_vec) < weight:
+            parity = random.randrange(k)
+            if column_vec[parity] == 0:
+                column_vec[parity] += 1
+        matrix_trans.append(column_vec)
+
+    matrix_trans = np.array(matrix_trans)
+    matrix = matrix_trans.T
+
+    ## 生成行列 matrix を表示する
+    matrix_list = matrix.tolist()
+    print("生成行列を表示します")
+    print(matrix_list)
+
+    ## matrix によって生成される符号語全体を表示する
+    code = []
+    for i in range(2 ** k):
+        code_word = []        
+        for l in range(n):  # 各行に対してループ
+            lth_ele = 0  # l列目と各 bit 列との演算結果を格納
+            for j in range(k):
+                if((i >> j) & 1):
+                    lth_ele = lth_ele + matrix[j][l]
+            lth_ele %= 2
+            code_word.append(lth_ele)
+        not_exists = True
+        for lis in code:
+            if(lis == code_word):
+                not_exists = False
+        if(not_exists == True):
+            code.append(code_word)
+    print("上記の生成行列によって生成される各符号語を表示します")
+    for code_word in code:
+        print(code_word)
+    
+
+def main() -> None:
+    args = sys.argv
+    if 3 == len(args):
+        LDPC_encode(args[1], args[2])
+    elif 2 >= len(args):
+        print('Arguments are too short')
+    else:
+        print('Arguments are too long')
+
+
+if __name__ == "__main__":
+    main()
